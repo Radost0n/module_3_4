f74761d8065952cfcea693e6c7d453422049b15e
Radost0n <154574169+Radost0n@users.noreply.github.com>
2024-10-27 00:10:39 +1000

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -14,7 +14,6 @@ def single_root_words(root_word, *other_words):
     return same_words
 
 
-# Примеры вызова функции
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 
